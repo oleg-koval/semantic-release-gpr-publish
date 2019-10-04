7bf3b0e9a5831729b51ab1351e9f9453708fd4cc
Oleg Koval <orthodoz@gmail.com>
2019-10-04 23:45:44 +0200

    feat(release): add workflows with configuration for GHPR

--- scripts/dry-run.sh
+++ scripts/dry-run.sh
@@ -6,4 +6,5 @@ prefix="refs/heads/"
 [[ "$GITHUB_REF" =~ ^$prefix(.*)$ ]]
 BRANCH=${BASH_REMATCH[1]}
 
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > /home/runner/work/_temp/.npmrc
 npx semantic-release --dry-run --debug -b $BRANCH
