6a4812ed48da8fcb18cb5141a43a15bfb12c87e6
Oleg Koval <orthodoz@gmail.com>
2019-10-04 23:53:11 +0200

    ci: debug

--- scripts/dry-run.sh
+++ scripts/dry-run.sh
@@ -6,5 +6,7 @@ prefix="refs/heads/"
 [[ "$GITHUB_REF" =~ ^$prefix(.*)$ ]]
 BRANCH=${BASH_REMATCH[1]}
 
+echo $BRANCH
+
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" > /home/runner/work/_temp/.npmrc
 npx semantic-release --dry-run --debug -b $BRANCH
